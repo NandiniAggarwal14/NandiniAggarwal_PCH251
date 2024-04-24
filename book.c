af4d69de1c7048aeae1be0794232449f1f49e417
NandiniAggarwal14 <152698318+NandiniAggarwal14@users.noreply.github.com>
2024-04-24 11:45:55 +0530

    Add files via upload

---
+++ book.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+struct book
+{
+char title[40];
+char author[40];
+float price;
+}b[20];
+int main()
+{
+int n,i,lg,sm;
+printf("\nInput the number of entries: ");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("\nInput the name of the book %d:",i+1);
+//fgets(b[i].title,40,stdin);
+scanf(" %[^\n]",b[i].title);
+printf("\nInput the author of book %d: ",i+1);
+//fgets(b[i].author,40,stdin);
+scanf(" %[^\n]",b[i].author);
+printf("\nInput the price of book %d: ",i+1);
+scanf("%f",&b[i].price);
+}
+sm=b[0].price;
+lg=b[0].price;
+for(i=0;i<n;i++)
+{
+if(b[i].price>lg)
+lg=i;
+if(b[i].price<sm)
+sm=i;
+}
+printf("\nThe book with the highest price is : %s %s %f",b[lg].title,b[lg].author,b[lg].price);
+printf("\nThe book with the lowest price is : %s %s %f",b[sm].title,b[sm].author,b[sm].price);
+return 0;
+}
